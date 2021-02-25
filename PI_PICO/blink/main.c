8ace98509693860505b83a05a96dd9751a480827
martin chaparro <mdchaparror@gmail.com>
2021-02-24 22:03:11 -0500

    blink pi_pico

--- PI_PICO/blink/main.c
+++ PI_PICO/blink/main.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
-#include "pico/binary_info.h"
 
 int main() {
-    stdio_init_all();
     const uint LED_PIN = 25;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+
     while (true) {
         gpio_put(LED_PIN, 1);
-        sleep_ms(250);
+        sleep_ms(100);
         gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+        sleep_ms(100);
     }
     return 0;
 }
